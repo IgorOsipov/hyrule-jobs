d133b4dde6561313b7356568da3d5f7a2aae187c
IgorOsipov <awesomeigor@gmail.com>
2022-08-14 23:27:18 +0300

    added sorting feature

--- src/components/JobList.vue
+++ src/components/JobList.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="job-list">
-    <p>Ordered by {{order}}</p>
+    <p>Ordered by {{ order }}</p>
     <ul>
-      <li v-for="job in jobs" :key="job.id">
+      <li v-for="job in orderJobs" :key="job.id">
         <h2>{{ job.title }} in {{ job.location }}</h2>
         <div class="salary">
           <p>{{ job.salary }}</p>
@@ -23,7 +23,7 @@
 <script lang="ts">
 import Job from "@/types/Job";
 import OrderTerm from "@/types/OrderTerm";
-import { defineComponent, PropType } from "vue";
+import { computed, defineComponent, PropType } from "vue";
 
 export default defineComponent({
   props: {
@@ -33,40 +33,49 @@ export default defineComponent({
     },
     order: {
       required: true,
-      type: String as PropType<OrderTerm>
-    }
+      type: String as PropType<OrderTerm>,
+    },
+  },
+  setup(props) {
+    const orderJobs = computed(() => {
+      return [...props.jobs].sort((a: Job, b: Job) => {
+        return a[props.order] > b[props.order] ? 1 : -1;
+      });
+    });
+
+    return { orderJobs };
   },
 });
 </script>
 
 <style scoped>
-  .job-list {
-    max-width: 960px;
-    margin: 40px auto;
-  }
-  .job-list ul {
-    padding: 0;
-  }
-  .job-list li {
-    list-style-type: none;
-    background: white;
-    padding: 16px;
-    margin: 16px 0;
-    border-radius: 4px;
-  }
-  .job-list h2 {
-    margin: 0 0 10px;
-    text-transform: capitalize;
-  }
-  .salary {
-    display: flex;
-  }
-  .salary img {
-    width: 30px;
-  }
-  .salary p {
-    color: #17bf66;
-    font-weight: bold;
-    margin: 10px 4px;
-  }
+.job-list {
+  max-width: 960px;
+  margin: 40px auto;
+}
+.job-list ul {
+  padding: 0;
+}
+.job-list li {
+  list-style-type: none;
+  background: white;
+  padding: 16px;
+  margin: 16px 0;
+  border-radius: 4px;
+}
+.job-list h2 {
+  margin: 0 0 10px;
+  text-transform: capitalize;
+}
+.salary {
+  display: flex;
+}
+.salary img {
+  width: 30px;
+}
+.salary p {
+  color: #17bf66;
+  font-weight: bold;
+  margin: 10px 4px;
+}
 </style>
